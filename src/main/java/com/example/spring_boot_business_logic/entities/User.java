006f9cf0613aae9d9829b66e568e95b4e7a3e216
Vugar Karimli <vuqarkrm1@gmail.com>
2025-04-19 05:40:40 +0400

    Initial commit

---
+++ src/main/java/com/example/spring_boot_business_logic/entities/User.java
@@ -0,0 +1,11 @@
+package com.example.spring_boot_business_logic.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+    private String password;
+    private String code;
+}
---
+++ src/test/java/com/example/spring_boot_business_logic/SpringBootBusinessLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_business_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBusinessLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
